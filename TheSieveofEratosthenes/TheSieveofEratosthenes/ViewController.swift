7c81a20dfc4528a943c825606bc68596a655ce72
MitchSamaniego <daniel.samaniego@nextia.mx>
2021-02-23 19:12:09 -0600

    First Commit

---
+++ TheSieveofEratosthenes/TheSieveofEratosthenes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheSieveofEratosthenes
+//
+//  Created by Mitch Samaniego on 23/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
